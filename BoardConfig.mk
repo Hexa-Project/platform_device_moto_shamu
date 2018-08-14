2c4753c8136a7b44313237d8882bf11847fe730e
KangDroid <hyunwoo286@gmail.com>
2018-08-14 20:36:41 +0900

    Fix prefix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_KERNEL_CONFIG := shamu_defconfig
 TARGET_KERNEL_SOURCE := kernel/moto/shamu
 BOARD_KERNEL_IMAGE_NAME := zImage-dtb
-KERNEL_TOOLCHAIN_PREFIX := arm-gnueabi-
+KERNEL_TOOLCHAIN_PREFIX := arm-linux-gnueabi-
 KERNEL_TOOLCHAIN := ./prebuilts/gcc/$(HOST_OS)-x86/arm/arm-linux-gnueabi/bin
 
 BOARD_KERNEL_BASE := 0x00000000
