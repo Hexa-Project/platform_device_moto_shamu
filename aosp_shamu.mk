7f2ca7b7f9d57ecb999bd5d4d0b4c2806a60ccff
KangDroid <hyunwoo286@gmail.com>
2018-08-17 01:25:22 +0900

    disable WITH_DEXPREOPT

--- aosp_shamu.mk
+++ aosp_shamu.mk
@@ -28,6 +28,7 @@ PRODUCT_DEVICE := shamu
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on Shamu
 PRODUCT_MANUFACTURER := motorola
+WITH_DEXPREOPT := false
 #PRODUCT_RESTRICT_VENDOR_FILES := true
 
 $(call inherit-product, device/moto/shamu/device.mk)
